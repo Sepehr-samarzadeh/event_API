8ca3cf6cdd373c8d17d96669bd8c6ea4d8c99d94
Sepehr Samarzadeh Vajdehfar <ssamarzadeh@gmail.com>
2025-05-21 00:34:30 +0200

    changes in project layout

---
+++ routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)   //set handler for get request
+	server.GET("events/:id", getEvent) //gin enable  developer to make dynamic endpoint with : + random name
+	server.POST("/events", createEvent)
+}
